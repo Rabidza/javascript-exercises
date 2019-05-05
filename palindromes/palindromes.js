88402663785a033c8d74e57b673b98fc109d64b8
Neill Henning <neillhenning@gmail.com>
2019-05-05 18:30:44 +0200

    Finished Palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  plainString = str.toLowerCase().replace(/[^A-Za-z]/g, "");
+  return (plainString.split("").reverse().join("") == plainString
+  );
 }
 
 module.exports = palindromes
