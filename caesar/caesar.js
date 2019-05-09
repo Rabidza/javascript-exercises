31579c42fe25534d5e8e8fe9ab91801735bfac92
Neill Henning <neillhenning@gmail.com>
2019-05-09 07:24:34 +0200

    Finished Caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,22 @@
-const caesar = function() {
-
+const caesar = function(str, shift) {
+  let arr = str.split('');
+  let shifted = arr.map(char => shiftTo(char, shift));
+  let newString = shifted.join('');
+  return newString;
 }
+
+const codeSet = code => (code < 97 ? 65 : 97);
+const alpha = (a, z) => (a % z + z) % z;
+
+const shiftTo = (char, shift) => {
+  const code = char.charCodeAt();
+  if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+      return String.fromCharCode(alpha(code + shift - codeSet(code), 26) + codeSet(code));
+  }
+  return char;
+};
+
+module.exports = caesar
+
 
 module.exports = caesar
