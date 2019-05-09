4f9d71d2dcfd7006db63a9f899d5147ba6115cf2
Neill Henning <neillhenning@gmail.com>
2019-05-09 07:42:21 +0200

    Finished Fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 1) return "OOPS";
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < n; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return b;
 
 }
 
